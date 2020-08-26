8fa76ee2ea8288f7101b5ea57a8315aadd3067d8
Alexis San Javier <alexissj16@knights.ucf.edu>
2020-08-26 01:57:39 -0400

    added favicon

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -4,21 +4,25 @@ import Book from "../Book";
 const BookList = ({ data }) => {
   return (
     <div className="container">
-      {data.map((book, i) => {
-        return (
-          <Book
-            key={i}
-            id={book._id}
-            bookId={book.bookId}
-            authors={book.authors}
-            description={book.description}
-            image={book.image}
-            link={book.link}
-            title={book.title}
-            saved={book.saved}
-          />
-        );
-      })}
+      {data.length > 0 ? (
+        data.map((book, i) => {
+          return (
+            <Book
+              key={i}
+              id={book._id}
+              bookId={book.bookId}
+              authors={book.authors}
+              description={book.description}
+              image={book.image}
+              link={book.link}
+              title={book.title}
+              saved={book.saved}
+            />
+          );
+        })
+      ) : (
+        <div className="text-center">No Books to Display :(</div>
+      )}
     </div>
   );
 };
--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -9,25 +9,30 @@ const Form = () => {
     if (bookContext.search.value) {
       API.searchBooks(bookContext.search.value)
         .then((books) => {
-          console.log(books);
-          let ordered = books.data.items.reduce((obj, book) => {
-            obj.push({
-              authors: book.volumeInfo.authors,
-              description: book.volumeInfo.description
-                ? book.volumeInfo.description
-                : "No Description",
-              image: book.volumeInfo.imageLinks
-                ? book.volumeInfo.imageLinks.thumbnail
-                : `https://via.placeholder.com/128x190`,
-              link: book.volumeInfo.infoLink,
-              title: book.volumeInfo.title,
-              saved: false,
-              bookId: book.id,
-            });
-            return obj;
-          }, []);
+          if (books.data.items) {
+            let ordered = books.data.items.reduce((obj, book) => {
+              obj.push({
+                authors: book.volumeInfo.authors
+                  ? book.volumeInfo.authors.join(",")
+                  : "No Authors",
+                description: book.volumeInfo.description
+                  ? book.volumeInfo.description
+                  : "No Description",
+                image: book.volumeInfo.imageLinks
+                  ? book.volumeInfo.imageLinks.thumbnail
+                  : `https://via.placeholder.com/128x190`,
+                link: book.volumeInfo.infoLink,
+                title: book.volumeInfo.title,
+                saved: false,
+                bookId: book.id,
+              });
+              return obj;
+            }, []);
 
-          bookContext.books.set(ordered);
+            bookContext.books.set(ordered);
+          } else {
+            bookContext.books.set([]);
+          }
         })
         .catch((err) => {
           console.log(err);
