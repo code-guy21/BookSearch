c573c2f196f597f1db5a49f536f535e239589a6b
Alexis San Javier <alexissj16@knights.ucf.edu>
2020-09-25 13:11:21 -0400

    removed text displaying no books

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -4,25 +4,21 @@ import Book from "../Book";
 const BookList = ({ data }) => {
   return (
     <div className="container">
-      {data.length > 0 ? (
-        data.map((book, i) => {
-          return (
-            <Book
-              key={i}
-              id={book._id}
-              bookId={book.bookId}
-              authors={book.authors}
-              description={book.description}
-              image={book.image}
-              link={book.link}
-              title={book.title}
-              saved={book.saved}
-            />
-          );
-        })
-      ) : (
-        <div className="text-center">No Books to Display :(</div>
-      )}
+      {data.map((book, i) => {
+        return (
+          <Book
+            key={i}
+            id={book._id}
+            bookId={book.bookId}
+            authors={book.authors}
+            description={book.description}
+            image={book.image}
+            link={book.link}
+            title={book.title}
+            saved={book.saved}
+          />
+        );
+      })}
     </div>
   );
 };
